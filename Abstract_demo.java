f42c1b63d526c002512c2bb27f0bd3c43e18e3df
nitingal <nitingalande11@gmail.com>
2022-04-08 15:02:35 +0530

    This is

---
+++ Abstract_demo.java
@@ -0,0 +1,15 @@
+package abstract_study;
+
+public abstract class Abstract_demo {
+public void test1()
+{
+	System.out.println("This is complete method of  test1 ");
+}
+public void test2()
+{
+	System.out.println("This is complete method of  test2");
+}
+public abstract void test3();
+
+
+}
---
+++ Concrete_study.java
@@ -0,0 +1,22 @@
+package abstract_study;
+
+public class Concrete_study extends Abstract_demo  {
+
+	public static void main(String[] args) {
+	Concrete_study c = new Concrete_study();
+	c.test1();
+	c.test2();
+	c.test3();
+	c.test4();
+
+	}
+public void test4()
+{
+	System.out.println("This is complete method of concrete class");
+}
+
+public void test3() {
+	// TODO Auto-generated method stub
+	
+}
+}
